a6cab93d8e8249bfb7f53e25777335535a99b10a
ComputerGuy11 <coltcomicsinc7667@gmail.com>
2024-11-30 19:38:14 -0500

    plzwork

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "GoofyGooberProductions-CompGuy-0.06";
+const contentToCache = [
+    "Build/compguyweb.loader.js",
+    "Build/compguyweb.framework.js",
+    "Build/compguyweb.data",
+    "Build/compguyweb.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
